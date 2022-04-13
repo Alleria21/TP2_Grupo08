6633b54c2f311e3b386dcacd727362f3a239d7cf
Rocio <aoi2011@hotmail.es>
2022-04-12 21:38:44 -0300

    creación de proyecto

---
+++ src/main/java/ar/edu/unju/fi/Tp2Grupo08Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo08Application.class, args);
+	}
+
+}
