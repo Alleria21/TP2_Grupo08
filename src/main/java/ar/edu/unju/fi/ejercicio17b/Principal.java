85cc9280eedbdc23a4e6a470f4fd724d5fb7437f
Rocio <roaguerrero17@gmail.com>
2022-04-16 19:04:22 -0300

    resolucion ejercicios 17b6-b7

--- src/main/java/ar/edu/unju/fi/ejercicio17b/Principal.java
+++ src/main/java/ar/edu/unju/fi/ejercicio17b/Principal.java
@@ -3,6 +3,9 @@ package ar.edu.unju.fi.ejercicio17b;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 
 import ar.edu.unju.fi.ejercicio17a.ProcesoFecha;
 
@@ -14,6 +17,7 @@ public class Principal {
 		ProcesoFecha pf4 = new ProcesoFecha(LocalDate.parse("2002-10-04"), LocalDate.now());
 		LocalTime horaPlus = pf2.getTime1().plusHours(5);
 		LocalTime horaMinus = pf2.getTime2().minusMinutes(20);
+		
 		
 		//Ejercicio b1
 		System.out.println(pf1.getFecha1()+"\n"+pf1.getFecha2());
@@ -31,8 +35,40 @@ public class Principal {
 		//Ejercicio b5
 		System.out.println(mostrarTiempoTranscurrido(pf4));
 		
-		//Ejercicio b6
+		//Ejercicio b6-b7
+		Scanner scanner = new Scanner(System.in);
+		try {
+			
+			System.out.println("Ingrese dd/MM/yyyy");
+			String ingreso;
+			ingreso=scanner.nextLine();
+			
+			DateTimeFormatter df= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			LocalDate fechaFormato = LocalDate.parse(ingreso,df); // nueva fecha con patron asignado
+			ProcesoFecha pf5 = new ProcesoFecha();
+			pf5.setFecha1(fechaFormato);
+			LocalDate fecha1Incrementado=pf5.getFecha1().plusDays(365);
+			System.out.println("La fecha incrementada en 365 dias es: " + fecha1Incrementado);
+			System.out.println("Ingrese otra fecha dd/MM/yyyy");
+			String otroIngreso=scanner.nextLine();
+			LocalDate otraFechaFormato = LocalDate.parse(otroIngreso,df);
+			pf5.setFecha2(otraFechaFormato);
+			LocalDate fecha2Incrementado=pf5.getFecha2().plusMonths(6);
+			System.out.println("La fecha incrementada en 6 meses es: "+fecha2Incrementado);
+		} catch (DateTimeParseException e) {
+			System.out.println("se ingreso un formato de fecha invalido");
+			e.getMessage();
+		}
+		
+		finally {
+			scanner.close();
+		}
+				
+			
+				
 	}
+	
+	
 	
 	private static String mostrarTiempoTranscurrido(ProcesoFecha fecha) {
 		int dia = fecha.contarTiempoTranscurrido().getDays();
@@ -43,4 +79,9 @@ public class Principal {
 		return "\nHan transcurrido "+anio+" anios, "+mes+" meses y "+dia+" dias desde la fecha "+fecha1Formateada
 		+" hasta la fecha "+fecha2Formateada+"\n";
 	}
+	
+	
+		
+	
+	
 }
